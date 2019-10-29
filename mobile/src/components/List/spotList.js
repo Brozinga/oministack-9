a459731201b179b6a682c1fc254e6ab611f657c1
Brozinga <fbrozinga@outlook.com>
2019-10-29 02:03:06 -0300

    adicionado mais alguns pontos do app

--- mobile/src/components/List/spotList.js
+++ mobile/src/components/List/spotList.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, FlatList, Image } from 'react-native'
 import api from '../../services/api'
 
 
@@ -13,7 +13,9 @@ const spotList = ({ tech }) => {
             const response = await api.get('/spots', {
                 params: { tech }
             })
+            console.log(response.data);
 
+            setSpots(response.data)
         }
 
         getAllDataTechs();
@@ -23,8 +25,24 @@ const spotList = ({ tech }) => {
 
     return (
         <View style={style.container}>
-            <Text style={style.title}>Empresas que usam </Text><Text style={style.bold}>{tech}</Text>
-        </View>
+            <Text style={style.title}>Empresas que usam <Text style={style.bold}>{tech}</Text></Text>
+            <FlatList style={style.list}
+                data={spots}
+                keyExtractor={spot => spot._id}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                renderItem={({ item }) => {
+                    <View style={style.listItem}>
+                        <Image style={style.thumbnail} source={{ uri: item.thumbnail_url }} />
+                        <Text style={style.company}>{item.company}</Text>
+                        <Text style={style.price}>{item.price ? `R$ ${item.price}/Dia` : 'Gratuito'}</Text>
+                        <TouchableOpacity style={style.buttom} onPress={() => { }}>
+                            <Text> Text style={style.buttonText}>Solicitação de Reserva</Text>
+                        </TouchableOpacity>
+                    </View>
+                }}
+            ></FlatList >
+        </View >
     )
 }
 
@@ -35,11 +53,15 @@ const style = StyleSheet.create({
         marginTop: 30
     },
     title: {
+        fontSize: 17,
         color: '#444',
         paddingHorizontal: 20,
         marginBottom: 15
     },
     bold: {
         fontWeight: 'bold'
+    },
+    thumbnail: {
+
     }
 })
